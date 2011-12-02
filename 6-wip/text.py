1892cc125b28eb7bb7fefe85a56d1b74401ceb53
nwang <whs_alex@163.com>
2011-12-02 15:56:29 +0800

    modify stage 6.

--- 6-wip/text.py
+++ 6-wip/text.py
@@ -1,17 +1,2 @@
 #!/usr/bin/python2.6
 
-import os
-import pickle
-
-filepath=os.path.join(os.path.abspath("."),"file")
-
-fd=file(filepath,"r")
-pickle_object=pickle.load(fd)
-fd.close()
-
-for line in pickle_object:
-    word=""
-    for sector in line:
-        word+=sector[0]*sector[1]
-    print word
-
