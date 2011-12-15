859cd22b3e74616f2354dfac6171126b723c1979
wanghs <wanghs@linux-beyp.site>
2011-12-15 14:03:55 +0800

    Modify stage 8 and add stage 8 wiki url.

--- 8-finish/text.py
+++ 8-finish/text.py
@@ -13,14 +13,15 @@ for li in filelines:
     unpattern=re.search("^un: '(.*)'",li)
     pwpattern=re.search("^pw: '(.*)'",li)
     if unpattern is not None:
-        uname=unpattern.groups()[0]
+        uname=unpattern.groups()[0].decode("string_escape")
         #print "User name string: %s" % uname
     if pwpattern is not None:
-        pword=pwpattern.groups()[0]
+        pword=pwpattern.groups()[0].decode("string_escape")
         #print "Pass word string: %s" % pword
 
-#Why always wrong...
-#print "User name: %s" % decompress(uname)
-#print "Pass word: %s" % decompress(pword)
-print uname
-print pword
+#Why always wrong... 
+#Becasuse it should be using decode("string_escape") first
+print "User name: %s" % decompress(uname)
+print "Pass word: %s" % decompress(pword)
+#print uname
+#print pword
