0a7d73a6d5706f2c81537af924a2527a6adea554
nwang <whs_alex@163.com>
2011-12-26 17:59:06 +0800

    Finish stage 12 and create 13.Seems 13 need 12.

---
+++ 12-finish/text.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# URL: http://www.pythonchallenge.com/pc/return/evil.html
+
+files={"0":"file1","1":"file2","2":"file3","3":"file4","4":"file5"}
+string={"0":"","1":"","2":"","3":"","4":""}
+
+def usage():
+    print '''
+    Nothing.
+    '''
+
+def main():
+   
+    fd=file('evil2.gfx','r')
+
+    #lines=[line for line in fd.readlines()]
+    lines=[line for line in fd.readlines()]
+    fd.close()
+ 
+    newfile=""
+    for li in lines:
+        newfile+=li
+
+    #split
+    for char in range(len(newfile)):
+        string[str(char%5)]+=newfile[char]
+
+    #Write to files
+    for key in files:
+        fd=file(files[key],"w")
+        fd.write(string[key])
+        fd.close()
+
+
+if __name__=='__main__':
+    main()
