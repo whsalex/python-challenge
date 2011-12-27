5e061f82e691d9942a12ee3dab4264f5b637c8f5
nwang <nwang@suse.com>
2011-12-27 18:04:42 +0800

    Finish stage 14.

---
+++ 15-wip/text.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# URL: http://www.pythonchallenge.com/pc/return/uzi.html
+
+def usage():
+    print '''
+    Nothing.
+    '''
+
+def main():
+    pass
+
+if __name__=='__main__':
+    main()
