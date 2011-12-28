d031d9667ca71a7fd6f235ac6e6e1d0a93eb8a54
nwang <nwang@suse.com>
2011-12-28 17:00:21 +0800

    Recommit - Finish stage 15.

---
+++ 16-wip/text.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# URL: http://www.pythonchallenge.com/pc/return/mozart.html
+
+
+def usage():
+    print '''
+    Nothing.
+    '''
+
+def main():
+    pass
+
+if __name__=='__main__':
+    main()
