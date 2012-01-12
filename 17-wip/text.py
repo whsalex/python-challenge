bf74b231b31533af7e3523b5ed6ed66e3c92ed6d
nwang <whs_alex@163.com>
2012-01-12 15:47:09 +0800

    Stage 17 getting stuck... temproary commit.

--- 17-wip/text.py
+++ 17-wip/text.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python
 # URL: http://www.pythonchallenge.com/pc/return/romance.html
+#The forth http://www.pythonchallenge.com/pc/def/linkedlist.php
+
+import urllib2,cookielib
+
+URL='http://www.pythonchallenge.com/pc/return/romance.html'
+cookieFile='cookies.file'
 
 def usage():
     print '''
@@ -7,7 +13,35 @@ def usage():
     '''
 
 def main():
-    pass
+    #Due to authenication
+    passdomain=urllib2.HTTPPasswordMgrWithDefaultRealm()
+    passdomain.add_password(None,URL,'huge','file')
+    handler_au=urllib2.HTTPBasicAuthHandler(passdomain)
+
+    #Handler the cookies
+    cJar = cookielib.LWPCookieJar()
+    handler_ck=urllib2.HTTPCookieProcessor(cJar)
+  
+    opener=urllib2.build_opener(handler_au,handler_ck)
+    
+    #opener.open
+    urllib2.install_opener(opener)
+
+    fp=urllib2.urlopen(URL)
+
+    lines=[line for line in fp.readlines()]
+
+    fp.close()
+
+    #for li in lines:
+    #    print li,
+
+    print "cJar: ",cJar
+
+    for int, cookie in enumerate(cJar):
+        print "%d - %s" % (int, cookie)
+
+    cJar.save(cookieFile)
 
 if __name__=='__main__':
     main()
