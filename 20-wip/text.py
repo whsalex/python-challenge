3359ce3557efb87a50221ff642dab6462e354cfd
nwang <whs_alex@163.com>
2012-02-06 17:39:43 +0800

    Stage 20 WIP.

--- 20-wip/text.py
+++ 20-wip/text.py
@@ -7,7 +7,9 @@
 import urllib,urllib2
 
 #urlPhoneBook='http://www.pythonchallenge.com/pc/phonebook.php'
-fatherAddr='http://www.pythonchallenge.com/pc/stuff/violin.php'
+#fatherAddr='http://www.pythonchallenge.com/pc/stuff/violin.php'
+URL_domain='http://www.pythonchallenge.com/pc/hex'
+URL_PIC='http://www.pythonchallenge.com/pc/hex/unreal.jpg'
 
 def usage():
     print '''
@@ -15,6 +17,7 @@ def usage():
     '''
 
 def httpAuth():
+    '''Using urllib2.Or using urllib open "http://usrname:passwd@url"'''
     passDomain=urllib2.HTTPPasswordMgrWithDefaultRealm()
     passDomain.add_password(None,URL_domain,'butter','fly')
     hander_au=urllib2.HTTPBasicAuthHandler(passDomain)
@@ -29,6 +32,7 @@ def downloadFile(url,name):
 
 
 def call_him():
+    ''' Useless. '''
     #phoneServer=xmlrpclib.ServerProxy(urlPhoneBook)
 
     #List all method in server
@@ -60,7 +64,27 @@ def call_him():
 
 def main():
     #call_him()
-    downloadFile('http://www.pythonchallenge.com/pc/hex/unreal.jpg','unreal.jpg')
+    #downloadFile('http://www.pythonchallenge.com/pc/hex/unreal.jpg','unreal.jpg')
+    httpAuth()
+    fd=urllib2.urlopen(URL_PIC)
+    info=fd.info()
+    #Find out the range hint.
+    range_val=info.getheaders("Content-Range")
+    print "Content-range header: %s" % range_val
+    #It shows "Content-range header: ['bytes 0-30202/2123456789']"
+
+#    #For check http headers. "http://en.wikipedia.org/wiki/List_of_HTTP_headers"
+#    header={'Range':'bytes=30237-2123456789'}
+#    req=urllib2.Request(URL_PIC,headers=header)
+#
+#    fp=urllib2.urlopen(req)
+#    lines=[line for line in fp.readlines()]
+#    print fp.info().getheaders("Content-Range")
+#    fp.close()
+#
+#    for i in lines:
+#        print i,
+    
 
 if __name__=='__main__':
     main()
