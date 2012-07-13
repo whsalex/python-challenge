4240e21269d56be0eb1167cc2849a76263c55728
nwang <whs_alex@163.com>
2012-07-13 16:23:33 +0800

    Finished stage 24.

---
+++ 25-wip/text.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+#URL: http://www.pythonchallenge.com/pc/hex/lake.html
+#butter@fly - username:passwd
+
+def usage():
+    print '''
+    Nothing.
+    '''
+
+def run():
+    pass
+
+
+def main():
+    print "  ===  start level 25  === "
+    run()
+    print "  ===  finish level 25  === "
+
+if __name__=='__main__':
+    main()
